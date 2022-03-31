9f7d9bfcd988f3816e25b407ee744a85a07b341c
Bob Monsour <bob.monsour@gmail.com>
2022-03-31 09:26:04 -0700

    Changed image reference technique for head

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
 // --- START, eleventy-img ---
 const Image = require("@11ty/eleventy-img");
+const path = require("path");
 
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(src, {
     widths: [300, 600, 1400],
     formats: ["webp", "jpeg"],
     outputDir: "_site/img",
+    filenameFormat: function (id, src, width, format) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+      return `${name}-${width}w.${format}`;
+    },
   });
 
   let imageAttributes = {

aec69259113f8d0d4832ba5ceaa2fea5b12cb497
Bob Monsour <bob.monsour@gmail.com>
2022-04-04 09:33:36 -0700

    Fix typo in form-success page reference

--- .eleventy.js
+++ .eleventy.js
@@ -36,7 +36,7 @@ module.exports = function (eleventyConfig) {
 
   // Filter to place confetti only on formsuccess page
   eleventyConfig.addFilter("confetti", function (value) {
-    if (value === "formsuccess") {
+    if (value === "form-success") {
       return `<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script><script src="/js/confetti.js"></script>`;
     }
   });
